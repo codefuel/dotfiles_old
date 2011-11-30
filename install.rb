681e6daa02786cb2a0eff74ea152b1ccda546a0c
[NAME] <[EMAIL]>
2011-11-30 00:18:07 -0800

    Install forces symlinks if file exists, warns and creates a backup file if file exists

--- install.rb
+++ install.rb
@@ -5,6 +5,12 @@ home = File.expand_path('~')
 Dir['*'].each do |file|
   next if file =~ /install/ || file =~ /README/
   target = File.join(home, ".#{file}")
-  `ln -ns #{File.expand_path file} #{target}`
+
+  if File.exists?(target)
+    puts "*** FILE EXISTS: Creating a backup file for #{target}"
+    `cp -r #{target} #{target}.#{Time.new.strftime("%Y%m%d")}`
+  end
+
+  `ln -fns #{File.expand_path file} #{target}`
 end
 
